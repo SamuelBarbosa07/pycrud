aa38f93d8a8bbba4c8dd06f8d8f9e1f2a7664d78
Samuel <samuelbarbosa0717@gmail.com>
2023-11-04 17:13:18 -0300

    Criação catalogos de filmes

---
+++ main.py
@@ -0,0 +1,16 @@
+# Classe principal do programa
+# Importa a classe Interface
+from interface import Interface
+from bd import BD
+
+# Classe principal do programa
+interface = Interface()
+
+opcao = ""
+while opcao != 0:
+    interface.logoTipo()
+    interface.mostraMenuPrincipal()
+    opcao = interface.selecionaOpcao([1, 2, 0])
+    interface.limpaTela()
+
+    banco = BD("catalogoFilmes.db")
